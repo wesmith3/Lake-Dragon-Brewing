9df204d9aac8cde3c5bde715beec7a7fabf191b1
Wesley <wesmith314@gmail.com>
2024-08-02 23:09:50 -0700

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Navbar from "./navbar";
+
 
 export default function Home() {
   return (
     <main className="bg-ldb-parchment">
-      <Navbar />
       <div className="bg-ldb-parchment">
         <h1 className="">Lake Dragon Brewing Co</h1>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,7 +16,12 @@ const config: Config = {
         'ldb-green': '#5F6F52',
         'ldb-blue': '#36454F',
         'ldb-orange': '#CD7F32',
-      }
+      },
+      boxShadow: {
+        'text-sm': '0 1px 3px rgba(0, 0, 0, 0.3)',  // Small text shadow
+        'text-md': '0 2px 5px rgba(0, 0, 0, 0.3)',  // Medium text shadow
+        'text-lg': '0 4px 6px rgba(0, 0, 0, 0.9)',  // Large text shadow
+      },
     },
   },
   plugins: [],
